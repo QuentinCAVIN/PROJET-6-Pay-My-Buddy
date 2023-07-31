883ab3d35ba57cb0d52bf3d9661a76ac49fa8953
Quentin CAVIN <quentin.cavin@gmail.com>
2023-07-31 15:11:23 +0200

    initialisation du projet avec Sring Boot

---
+++ src/main/java/com/paymybuddy/paymybuddysapp/PaymybuddysappApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.paymybuddysapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymybuddysappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymybuddysappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymybuddy/paymybuddysapp/PaymybuddysappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.paymybuddysapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddysappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
