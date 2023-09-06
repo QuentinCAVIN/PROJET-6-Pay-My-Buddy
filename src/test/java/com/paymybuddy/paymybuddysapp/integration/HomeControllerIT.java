96b93b8fccd0caeea8d0e4b6b3e2b77069d866ba
Quentin CAVIN <quentin.cavin@gmail.com>
2023-09-06 16:49:44 +0200

    Avancé test

--- src/test/java/com/paymybuddy/paymybuddysapp/integration/HomeControllerIT.java
+++ src/test/java/com/paymybuddy/paymybuddysapp/integration/HomeControllerIT.java
@@ -1,24 +1,87 @@
 package com.paymybuddy.paymybuddysapp.integration;
 
+import com.paymybuddy.paymybuddysapp.model.User;
+import com.paymybuddy.paymybuddysapp.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
+
+
+
 public class HomeControllerIT {
 
-    /*
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static User currentUser;
+    @BeforeAll
+    public static void  setupBeforeAll (){
+        currentUser = new User();
+        currentUser.setId(1);
+        currentUser.setEmail("currentUser@test");
+        currentUser.setFirstName("test");
+        currentUser.setLastName("tset");
+        currentUser.setPassword("1234");
+    }
+
+    @BeforeEach
+    public void setupBeforeEach(){
+        userRepository.deleteAll();
+        userRepository.save(currentUser);
+    }
+
     @Test
     @WithMockUser("currentUser@test")
-    @DisplayName("An authenticated user should access his home page")
-    public void authenticatedUserShouldAccessHisHomePage() throws Exception {
-        userRepository.save(currentUser);
+    @DisplayName("/home should display home page")
+    @Transactional
+    public void HomeShouldDisplayHomePage() throws Exception {
+
         mockMvc.perform(MockMvcRequestBuilders.get("/home"))
 
                 .andExpect(MockMvcResultMatchers.view().name("home"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.model().attributeExists("users"));
-    }*/
+                .andExpect(MockMvcResultMatchers.model().attributeExists("buddies"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("personalBankAccountToFile"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("personalBankAccountToDisplay"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("payMyBuddyBankAccount"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("transfer"));
+    }
+
+    //TODO: Ci-dessous réaliser les tests
+    @WithMockUser("currentUser@test")
+    @Test
+    public void linkPersonalBankAccountTest()throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.post("/home/linkPersonalBankAccount")
+                .param("iban", "666")
+                .param("accountBalance","1000"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+    }
+
+    @WithMockUser("currentUser@test")
+    @Test
+    public void creditAndCashInTest()throws Exception{
+        linkPersonalBankAccountTest();
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/home/creditAndCashIn")
+                .param("button","credit"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+
 }
